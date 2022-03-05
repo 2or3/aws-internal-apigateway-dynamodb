b35b28b52adf1decf1df02607e6c0290ddc378a4
2or3 <t.nagashima.cog@gmail.com>
2022-03-06 01:24:13 +0900

    add api-gateway and dyanmodb

--- lib/aws-apigateway-dynamodb-stack.d.ts
+++ lib/aws-apigateway-dynamodb-stack.d.ts
@@ -1,5 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
 export declare class AwsApigatewayDynamodbStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps);
 }
--- lib/aws-apigateway-dynamodb-stack.ts
+++ lib/aws-apigateway-dynamodb-stack.ts
@@ -1,5 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { AttributeType, BillingMode } from '@aws-cdk/aws-dynamodb';
+
+import { ApiGatewayToDynamoDBProps, ApiGatewayToDynamoDB } from "@aws-solutions-constructs/aws-apigateway-dynamodb";
+import { EndpointType } from '@aws-cdk/aws-apigateway';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsApigatewayDynamodbStack extends Stack {
@@ -7,10 +11,16 @@ export class AwsApigatewayDynamodbStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const apigw_dynamodb_props: ApiGatewayToDynamoDBProps = {
+      dynamoTableProps: {
+        partitionKey: {name: "id", type: AttributeType.STRING },
+        removalPolicy: RemovalPolicy.DESTROY,
+      },
+      apiGatewayProps: {
+        restApiName: "DynamodbApi",
+      }
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsApigatewayDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ApiGatewayToDynamoDB(this, "ApiGatewayDynamodb", apigw_dynamodb_props);
   }
 }
