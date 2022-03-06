7c65107dae4a735d8678bdb4f484ae1bb520a41e
2or3 <t.nagashima.cog@gmail.com>
2022-03-07 00:08:37 +0900

    Remove d.ts file

--- lib/aws-apigateway-dynamodb-stack.d.ts
+++
@@ -1,4 +0,0 @@
-import { Construct, Stack, StackProps } from '@aws-cdk/core';
-export declare class AwsApigatewayDynamodbStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps);
-}
